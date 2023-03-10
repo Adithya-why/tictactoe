80903d139be6570f5149c343945a74ee34c4f85f
Adi <adithiyans123@gmail.com>
2023-03-10 10:45:37 +0530

    added a tie finder function

--- main.js
+++ main.js
@@ -8,7 +8,9 @@ const gameBoard = (function(){
             let i,j;
             i = digs[0];
             j=digs[1];
+            
             div.innerHTML = arr[i][j]
+            
         });
     };
 
@@ -17,32 +19,57 @@ const gameBoard = (function(){
         arrangefunc();
     };
 
-    return {arr,arrangefunc,putMark};
+    let findTie = function(){
+        let t = true;
+        for(i=0;i<arr.length;i++){
+            for(j=0;j<arr[0].length;j++){
+                if(arr[i][j]===undefined){
+                    t = false;
+                }
+            }
+        }
+
+        console.log(t);
+
+
+
+    };
+
+
+    let findWin = function(){
+        
+    }
+
+    return {arr,arrangefunc,putMark,findTie};
 })();
-
-
 
 
 
 const game = function(player1,player2,gameBoard){
     turn = player1;
+    console.log(turn);
     const divs = document.querySelectorAll(".r");
     divs.forEach((div)=>{
         div.addEventListener('click',function(e){
           let digs=(e.target.classList)[1].toString().split("");
-            console.log(turn.sign)
-          gameBoard.putMark(turn,digs[0],[digs[1]]);
+          gameBoard.putMark(turn,digs[0],digs[1]);
+          gameBoard.findTie();
+
           if(turn == player1){
             turn = player2;
+            console.log(turn);
           } 
 
           else{
             turn=player1;
+            console.log(turn);
           }
         });
 
     })
 }
+
+
 
 
 const player = (sign)=>{
